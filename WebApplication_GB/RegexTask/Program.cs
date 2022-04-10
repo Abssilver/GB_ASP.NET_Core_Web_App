384a19c06ca9bdd205e485c915d53e12df495f2d
Pavel Remizov <remizov.p1@azurgames.com>
2022-04-10 19:46:06 +0300

    Regex task

--- WebApplication_GB/RegexTask/Program.cs
+++ WebApplication_GB/RegexTask/Program.cs
@@ -11,9 +11,16 @@ namespace RegexTask
             string str = " Предложение один Теперь предложение два Предложение три ";
 
             RegexOptions options = RegexOptions.None;
-            Regex regex = new Regex("[ ]{2,}", options);
-            string result = regex.Replace(str, " ").Trim();
-            Console.WriteLine(result);
+            var regexRemoveExtraSpaces = new Regex("[ ]{2,}", options);
+            var regexPlaceDotsBeforeCapitalLetters = new Regex(@"(?=\s+[A-ZА-Я]\w*)\s+", options);
+            var regexPointInTheEnd = new Regex(@"[^\w.!?]$", options);
+            var resultNoExtraSpaces = regexRemoveExtraSpaces.Replace(str, " ");
+            var resultEndStringWithPoint = regexPointInTheEnd.Replace(resultNoExtraSpaces, ".").Trim();
+            var resultAddDots = regexPlaceDotsBeforeCapitalLetters.Replace(resultEndStringWithPoint, ". ");
+            Console.WriteLine($"Origin: \"{str}\"");
+            Console.WriteLine($"Remove extra spaces: \"{resultNoExtraSpaces}\"");
+            Console.WriteLine($"Place dot to the end: \"{resultEndStringWithPoint}\"");
+            Console.WriteLine($"Add dots into string body: \"{resultAddDots}\"");
             Console.ReadKey();
         }
     }
